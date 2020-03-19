a6604653aa4f9ff4fef35f02c0caef10e9463e50
1151757358@qq.com <1151757358@qq.com>
2020-03-19 13:50:15 +0800

    文件列表
    文件列表

--- SpringBoot-Upload/src/main/java/com/animo/upload/controller/AttachmentController.java
+++ SpringBoot-Upload/src/main/java/com/animo/upload/controller/AttachmentController.java
@@ -1,14 +1,19 @@
 package com.animo.upload.controller;
 
 
+import com.animo.upload.entity.AttachmentEntity;
 import com.animo.upload.service.IAttachmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -19,16 +24,60 @@ import java.util.Map;
  * @author 刘金泳
  * @since 2020-03-19
  */
-@RestController
+@Controller
 @RequestMapping("/attachment")
 public class AttachmentController {
+
+    @Value("${uploadFile.location}")
+    private String location;
+
 
     @Autowired
     private IAttachmentService attachmentService;
 
     @PostMapping("/upload")
+    @ResponseBody
     public Map<String,Object> upload(MultipartFile file){
         return attachmentService.upload(file);
+    }
+
+    @GetMapping("/download/{id}")
+    public void getFile(@PathVariable("id")String id, HttpServletResponse response){
+        AttachmentEntity attachmentEntity = attachmentService.getById(id);
+        File file = new File(location + attachmentEntity.getFileAlias());
+        try {
+            InputStream fis = new BufferedInputStream(new FileInputStream(file));
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            fis.close();
+            response.reset();
+            // 设置response的Header
+            response.addHeader("Content-Length", "" + file.length());
+            response.setContentType(attachmentEntity.getContentType());
+            response.setHeader("Content-Disposition", "attachment;filename=" + java.net.URLEncoder.encode(attachmentEntity.getFileName(), "UTF-8"));
+            OutputStream toClient = new BufferedOutputStream(response.getOutputStream());
+            toClient.write(buffer);
+            toClient.flush();
+            toClient.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/listData")
+    @ResponseBody
+    public Map<String,Object> listData(){
+        Map<String,Object> map = new HashMap<>();
+        map.put("code",0);
+        map.put("msg","");
+        map.put("count",1000);
+        map.put("data",attachmentService.list());
+        return map;
+    }
+
+    @GetMapping("/list")
+    public String list(){
+        return "list";
     }
 
 }
